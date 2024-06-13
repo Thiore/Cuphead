e24ca60e72ff9cfa7a64aabcdb1565de64839456
Thiore <93072368+Thiore@users.noreply.github.com>
2024-06-13 21:01:37 +0900

    Dust_FX완성

---
+++ Assets/3.Script/ETC/OverlayEffect.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+[RequireComponent(typeof(Camera))]
+public class OverlayEffect : MonoBehaviour
+{
+    [SerializeField] private Material FXOverlayMaterial;
+    [SerializeField] private Texture2D[] FXTex;
+    public float animationSpeed = 10.0f;  // 프레임 변경 속도
+    private float currentFrame;
+    private int lastFrame;
+
+    void Start()
+    {
+        currentFrame = 0;
+        lastFrame = 0;
+        FXOverlayMaterial.SetTexture("_DustTex", FXTex[0]);
+    }
+
+    void Update()
+    {
+        currentFrame += animationSpeed * Time.deltaTime;
+
+        if (currentFrame >= 127) 
+            currentFrame = 0;
+
+        
+
+        if(!lastFrame.Equals(Mathf.FloorToInt(currentFrame)))
+        {
+            lastFrame = Mathf.FloorToInt(currentFrame);
+            SetFXTex(lastFrame);
+        }
+
+
+        FXOverlayMaterial.SetFloat("_DustFrame", currentFrame);
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (FXOverlayMaterial != null)
+        {
+            Graphics.Blit(src, dest, FXOverlayMaterial);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+
+    private void SetFXTex(int currentFrame)
+    {
+        FXOverlayMaterial.SetTexture("_DustTex", FXTex[currentFrame]);
+    }    
+}
